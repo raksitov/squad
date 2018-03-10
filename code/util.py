6150fe437a774e708798fbae503f1f6f0bc4adb7
Renat Aksitov <r.aksitov@gmail.com>
2018-03-09 21:51:42 -0800

    Support parsing of hparams from the experiment name

--- code/util.py
+++ code/util.py
@@ -6,14 +6,32 @@ import tensorflow as tf
 def params_dict():
   return tf.flags.FLAGS.__flags
 
-def hparams_to_str():
-  hparams_names = [name for name in sorted(params_dict().keys()) if
+def hparams_names():
+  return [name for name in sorted(params_dict().keys()) if
       name.startswith('h_')]
-  ordered_values = [str(params_dict()[name]) for name in hparams_names]
+
+def hparams_to_str():
+  ordered_values = [str(params_dict()[name]) for name in hparams_names()]
   return ':'.join(ordered_values)
+
+def convert(value):
+  if value[0].isalpha():
+    return str(value)
+  if value.find('.') != -1:
+    return float(value)
+  return int(value)
+
+def maybe_parse(name):
+  values = name.split(':')
+  if len(values) != len(hparams_names()):
+    return False
+  for idx, name in enumerate(hparams_names()):
+    params_dict()[name] = convert(values[idx])
+  return True
 
 def experiment_name(experiment_name):
   if experiment_name:
+    maybe_parse(experiment_name)
     return experiment_name
   return hparams_to_str()
 
