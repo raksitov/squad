8d376b22cce92e7b4db84dbc73b7742d7c1f2029
Renat Aksitov <r.aksitov@gmail.com>
2018-03-13 00:40:58 -0700

    Add RNN for predicting span ends

--- code/main.py
+++ code/main.py
@@ -65,6 +65,7 @@ tf.app.flags.DEFINE_string("experiments_results", os.path.join(DEFAULT_DATA_DIR,
 tf.app.flags.DEFINE_bool("train_loss", False, "Adds train loss graph calculated over the whole train dataset")
 tf.app.flags.DEFINE_bool("modeling_layer_uses_rnn", True, "Use RNN for modelling layer instead of FF")
 tf.app.flags.DEFINE_bool("use_bidaf", False, "Whether to use basic attention or bidaf.")
+tf.app.flags.DEFINE_bool("use_rnn_for_ends", False, "Whether to use rnn for predicting span ends.")
 
 # How often to print, save, eval
 tf.app.flags.DEFINE_integer("print_every", 1, "How many iterations to do per print.")
--- code/qa_model.py
+++ code/qa_model.py
@@ -187,6 +187,12 @@ class QAModel(object):
         # Use softmax layer to compute probability distribution for end location
         # Note this produces self.logits_end and self.probdist_end, both of which have shape (batch_size, context_len)
         with vs.variable_scope("EndDist"):
+            if self.FLAGS.use_rnn_for_ends:
+              end_encoder = RNNEncoder(self.FLAGS.h_hidden_size, self.keep_prob,
+                num_layers=self.FLAGS.h_num_layers,
+                cell_type=self.FLAGS.h_cell_type, scope='blended_reps_final')
+              blended_reps_combined = tf.concat([blended_reps_final, tf.expand_dims(self.probdist_start, 2)], 2)
+              blended_reps_final = end_encoder.build_graph(blended_reps_combined, self.context_mask)
             softmax_layer_end = SimpleSoftmaxLayer()
             self.logits_end, self.probdist_end = softmax_layer_end.build_graph(blended_reps_final, self.context_mask)
 
